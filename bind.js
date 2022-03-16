9094ef837769add656710f0d70ee847ed091ddaa
Juanto77 <58269289+Juanto77@users.noreply.github.com>
2022-03-16 15:34:02 -0500

    Finished JS interation to UI

--- bind.js
+++ bind.js
@@ -1,32 +1,53 @@
-//Operator button binding
+//Button Binding
 const divideBtn = document.getElementById('btnDivide');
 const multiplyBtn = document.getElementById('btnMultiply');
 const addBtn = document.getElementById('btnAdd');
 const subtractBtn = document.getElementById('btnSubtract');
 const equalBtn = document.getElementById('btnEqual');
-const number = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operator');
-//GUI
+
+//Display
 const result = document.getElementById('result');
 const display = document.getElementById('input');
-const currentResultOutput = document.getElementById('result');
-const currentCalculationOutput = document.getElementById('input');
 
-//stores user input for numbers and stores value in currentResult.
-const userNumber = number.forEach((i) =>
+//Number binding, click event listener & function
+const number = document.querySelectorAll('.number');
+number.forEach((i) =>
   i.addEventListener('click', (e) => {
-    secondInput += e.currentTarget.value;
-    // display.textContent += e.currentTarget.value;
+    numberClick(e);
   })
 );
 
-//stores user input for operators and stores value in currentResult.
-const userOperator = operators.forEach((i) =>
+//Operator Button Binding, Click event listener & function
+const operators = document.querySelectorAll('.operator');
+operators.forEach((i) =>
   i.addEventListener('click', (e) => {
-    currentOperator = e.currentTarget.value;
-    firstInput = secondInput;
-    secondInput = 0;
-    getSum();
-    // display.textContent += e.currentTarget.value;
+    operatorClick(e);
   })
 );
+
+//Display Number & Operator Clicks
+function numberClick(e) {
+  secondInput += e.currentTarget.value;
+  display.textContent += e.currentTarget.value;
+}
+
+function operatorClick(e) {
+  currentOperator = e.currentTarget.value;
+  display.textContent = e.currentTarget.value;
+  operate();
+  return calcOutput == 0 ? firstOperator() : continueOperation();
+}
+
+function firstOperator() {
+  firstInput = +secondInput;
+  secondInput = 0;
+}
+
+function continueOperation() {
+  secondInput = 0;
+}
+
+function updateSum() {
+  firstInput = +calcOutput;
+  result.textContent = calcOutput;
+}
--- calculator.js
+++ calculator.js
@@ -1,67 +1,26 @@
 const startingNumber = 0;
-let firstInput = startingNumber;
+let firstInput = 0;
 let secondInput = 0;
 let calcOutput = 0;
 let currentOperator = '';
 
-//This will call proper function depending on which operator is selected and get the sum of the numbers entered
-function operatorAssignment() {
-  return currentOperator == '+'
-    ? add()
-    : currentOperator == '-'
-    ? subtraction()
-    : currentOperator == '*'
-    ? multiply()
-    : divide();
-}
-
-function add() {
-  calcOutput += +secondInput;
-}
-
-function subtraction() {
-  return calcOutput !== 0
-    ? (calcOutput -= +secondInput)
-    : (calcOutput = +firstInput - +secondInput);
-}
-
-function divide() {
-  return calcOutput !== 0
-    ? (calcOutput /= +secondInput)
-    : (calcOutput = +firstInput / +secondInput);
-}
-
-function multiply() {
-  calcOutput *= secondInput;
-}
-
-function clearDisplay() {
-  display.textContent = `${parseInt(firstInput)} ${parseInt(secondInput)}`;
-}
-
-function declareResult() {
+function operate(operator, num1, num2) {
+  operator = currentOperator;
+  num1 = +firstInput;
+  num2 = +secondInput;
+  if (operator == '+') {
+    calcOutput = num1 + num2;
+  } else if (operator == '-') {
+    calcOutput = num1 - num2;
+  } else if (operator == '/' && num1 !== 0 && num2 !== 0) {
+    calcOutput = num1 / num2;
+  } else if (operator == '*') {
+    calcOutput = num1 * num2;
+  } else {
+    alert('Not a Number');
+  }
+  firstInput = +calcOutput;
   result.textContent = calcOutput;
 }
 
-//Reset the calculator
-function resetCalc() {
-  calcOutput = 0;
-  firstInput = 0;
-  secondInput = 0;
-  currentOperator = '';
-}
-
-function getSum() {
-  if (calcOutput == 0) {
-    calcOutput = +firstInput;
-    clearDisplay();
-    declareResult();
-  }
-  operatorAssignment();
-  clearDisplay();
-  declareResult();
-}
-
-equalBtn.addEventListener('click', getSum);
-// addBtn.addEventListener('click', add);
-// multiplyBtn.addEventListener('click', multiply);
+equalBtn.addEventListener('click', operate);
